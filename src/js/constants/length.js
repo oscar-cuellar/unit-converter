0b1d1ffe8a63718597bb340df4ace412711d5bb9
alan-serrano <alan.serrano.pch@gmail.com>
2024-02-10 20:00:04 -0400

    Crear el listado de unidades de medida

--- src/js/constants/length.js
+++ src/js/constants/length.js
@@ -3,3 +3,21 @@ export const METERS = 'm';
 export const MILLIMETERS = 'mm';
 export const DECIMETERS = 'dm';
 
+export const lengthUnits = [
+  {
+    id: CENTIMETERS,
+    name: 'Centímetros',
+  },
+  {
+    id: METERS,
+    name: 'Metros',
+  },
+  {
+    id: MILLIMETERS,
+    name: 'Milímetros',
+  },
+  {
+    id: DECIMETERS,
+    name: 'Decímetros',
+  }
+];
