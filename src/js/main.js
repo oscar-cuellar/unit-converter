74b228e726cfbe4ee9af03e955ff977207fbee8a
Oscar Cuellar <oscardariocuellar16@gmail.com>
2024-02-14 01:18:46 -0400

    Añadiendo las opciones del Select

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import { lengthUnits } from "../js/constants/length.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Adding length options
+  const selects = document.getElementsByClassName("select");
+  for (const select of selects) {
+    for (const { name, id } of lengthUnits) {
+      select.add(new Option(name , id));
+    }
+  }
+});
