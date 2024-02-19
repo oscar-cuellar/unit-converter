7b6aa5cf05d61741da75b0ce5b8fa99afb63fb3b
Oscar Cuellar <oscardariocuellar16@gmail.com>
2024-02-18 23:59:24 -0400

    Fix varios

--- src/js/helpers/getConvertionFunction.js
+++ src/js/helpers/getConvertionFunction.js
@@ -1,12 +1,16 @@
 /**
- * 
- * @param {Object} conversionObject //Objeto de la conversión
+ *
+ * @param {Object} conversionObject Objeto de la conversión
  * @param {String} sourceUnit
- * @param {String} targetUnit 
- * @returns 
+ * @param {String} targetUnit
+ * @return {Function}
  */
-export function getConvertionFunction(conversionObject, sourceUnit, targetUnit) {
-  if ( !conversionObject[sourceUnit]?.[targetUnit] ) {
+export function getConvertionFunction(
+  conversionObject,
+  sourceUnit,
+  targetUnit
+) {
+  if (!conversionObject[sourceUnit]?.[targetUnit]) {
     throw new Error('Las unidades de medida no existen');
   }
   return conversionObject[sourceUnit][targetUnit];
